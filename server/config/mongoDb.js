fb75b7580a6277148afbe46128e524fe20486178
Yugesh <yugeshtg@gmail.com>
2025-10-18 19:32:47 +0530

    reveting

--- server/config/mongoDb.js
+++ server/config/mongoDb.js
@@ -1,19 +1,13 @@
 import { connect } from 'mongoose';
 
 const connectDB = async ()=>{
-    const uri = process.env.MONGODB_URI;
-    if (!uri) {
-        console.log("DB connection failed !", new Error("Missing MONGODB_URI environment variable"));
-        return;
-    }
-
     try {
-        await connect(uri);
+        await connect("mongodb+srv://chandanusmangani_db_user:2NDdhx9Ky5IKYWgP@cluster0.irgcxyy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
         console.log("DB connected !");
         
     } catch (error) {
         console.log("DB connection failed !",error)       
     }
 }
-
+// 2NDdhx9Ky5IKYWgP
 export default connectDB
